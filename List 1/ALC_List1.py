8c3e0a5a508dde1d36d4c2cd9f7801b844db4204
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-09-10 22:46:30 -0300

    [List 1] Bugfix. Done.

--- List 1/ALC_List1.py
+++ List 1/ALC_List1.py
@@ -3,13 +3,13 @@ import copy
 
 def lu_decomposition(matrix):
     if(Matrix_Utils.matrix_determinant(matrix) == 0):
-        return -1;
+        return "Error"
 
     number_of_rows    = len(matrix)
     number_of_columns = len(matrix[0])
 
     if(number_of_rows != number_of_columns):
-        return -1
+        return "Error"
 
     result = copy.deepcopy(matrix)
 
@@ -29,11 +29,11 @@ def cholesky_decomposition(matrix):
     number_of_columns = len(matrix[0])
 
     if(number_of_rows != number_of_columns):
-        return -1
+        return "Error"
 
     #Check if the matrix is simetric, if not, return -1
     if(not Matrix_Utils.positive_definite(matrix)):
-        return -1
+        return "Error"
 
     l = [[0.0] * len(matrix) for _ in range(len(matrix))]
 
@@ -53,11 +53,9 @@ def cholesky_decomposition(matrix):
 
 def solve_by_lu_decomposition(matrix_a, matrix_b):
     matrix_lu = copy.deepcopy(matrix_a)
-
     matrix_lu = lu_decomposition(matrix_lu)
-
-    if(matrix_lu == -1):
-        return -1
+    if(matrix_lu == "Error"):
+        return "Error"
 
     matrix_y = Matrix_Utils.forward_substitution_(matrix_lu, matrix_b)
     return Matrix_Utils.backward_substitution(matrix_lu, matrix_y)
@@ -66,58 +64,58 @@ def solve_by_lu_decomposition(matrix_a, matrix_b):
 def solve_by_cholesky_decomposition(matrix_a, matrix_b):
     matrix_lu = copy.deepcopy(matrix_a)
     matrix_lu = cholesky_decomposition(matrix_lu)
-    if(matrix_lu == -1):
-        return -1
+    if(matrix_lu == "Error"):
+        return "Error"
 
     matrix_y = Matrix_Utils.forward_substitution(matrix_lu, matrix_b, True)
-    print(matrix_y)
     return Matrix_Utils.backward_substitution(Matrix_Utils.get_transposed_matrix(matrix_lu), matrix_y)
 
 
 def iterative_jacobi(matrix_a, matrix_b):
     if (not Matrix_Utils.converge(matrix_a)):
-        return -1
+        return "Error"
 
     n = len(matrix_a)
 
-    solution_zero = [1.0 for i in range(n)]
+    solution_zero = [0.0 for i in range(n)]
     next_solution = [0.0 for i in range(n)]
 
-    tol     = 10**(-3)
-    residue = tol + 1
-
-    i           = 0
-    numerator   = 0
-    denominator = 0
+    tol      = 10**(-3)
+    residue  = 1
+    step     = 0
 
     while (residue > tol):
 
+        numerator   = 0
+        denominator = 0
+
         for j in range(n):
-            c = 0
+            next_solution[j] = matrix_b[j]
 
             for k in range(n):
                 if (j!=k):
-                    c += (matrix_a[j][k] * solution_zero[k])
+                    next_solution[j] += (-1)*(matrix_a[j][k] * solution_zero[k])
 
-                next_solution[j] = (matrix_b[j]-c)/matrix_a[j][j]
+            next_solution[j] /= matrix_a[j][j]
 
         for z in range(n):
             numerator   += (next_solution[z]-solution_zero[z])**2
             denominator += next_solution[z]**2
 
-        residue       = float(numerator**0.5)/(denominator**0.5)
-        solution_zero = next_solution
+        residue = float(numerator**0.5)/(denominator**0.5)
 
-        i += 1
+        for i in range(len(next_solution)):
+            solution_zero[i] = next_solution[i]
+        step+=1
 
     print("x1: "              , next_solution)
     print("residue: "         , residue      )
-    print("iteration_number: ", i            )
+    print("iteration_number: ", step         )
 
-
+    
 def gauss_seidel(matrix_a, matrix_b):
     if (not Matrix_Utils.converge(matrix_a)):
-        return -1
+        return "Error"
 
     n = len(matrix_a)
 
@@ -125,15 +123,14 @@ def gauss_seidel(matrix_a, matrix_b):
     next_solution = [0.0 for i in range(n)]
 
     tol     = 10**(-3)
-    residue = tol + 1
-
-    i                = 0
-    numerator        = 0
-    denominator      = 0
-    second_summation = 0
-    second_summation = 0
+    residue = 1
+    step    = 0
 
     while (residue > tol):
+        numerator        = 0
+        denominator      = 0
+        second_summation = 0
+        second_summation = 0
 
         for j in range(n):
             first_summation  = Matrix_Utils.sum_of_vector_multiplication(matrix_a[j][:j], next_solution[:j])
@@ -144,11 +141,15 @@ def gauss_seidel(matrix_a, matrix_b):
             numerator   += (next_solution[z]-solution_zero[z])**2
             denominator += next_solution[z]**2
 
-        residue       = float(numerator**0.5)/(denominator**0.5)
-        solution_zero = next_solution
+        residue = float(numerator**0.5)/(denominator**0.5)
 
-        i += 1
+        for i in range(len(next_solution)):
+            solution_zero[i] = next_solution[i]
+
+        print(next_solution, residue)
+
+        step += 1
 
     print("x1: "              , next_solution)
     print("residue: "         , residue      )
-    print("iteration_number: ", i            )
+    print("iteration_number: ", step         )
