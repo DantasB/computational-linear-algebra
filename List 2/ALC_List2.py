dd7c4772465f54455f019d99363e5cf4d42c73f1
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-09-11 22:51:23 -0300

    [List 2] Bugfix. Done.
    Fixing jacobi_method, now it returns the correct result.

--- List 2/ALC_List2.py
+++ List 2/ALC_List2.py
@@ -42,19 +42,23 @@ def jacobi_method(matrix):
 
     number_of_rows  = len(matrix)
     identity_matrix = [[float(i==j) for j in range(number_of_rows)] for i in range(number_of_rows)]
-    tol             = 10**(-2)
+    tol             = 10**(-3)
     biggest_element = Matrix_Utils.get_biggest_element(matrix)
+    steps           = 1
 
-    while (matrix[biggest_element[0]][biggest_element[1]] > tol):
+    while (math.fabs(matrix[biggest_element[0]][biggest_element[1]]) > tol):
         p_matrix            = Matrix_Utils.calculate_p_matrix(matrix, biggest_element)
         p_matrix_transposed = Matrix_Utils.get_transposed_matrix(p_matrix)
         matrix              = Matrix_Utils.multiply_matrixes(p_matrix_transposed, Matrix_Utils.multiply_matrixes(matrix, p_matrix))
         identity_matrix     = Matrix_Utils.multiply_matrixes(identity_matrix, p_matrix)
         biggest_element     = Matrix_Utils.get_biggest_element(matrix)
 
-    autovalores = []
+        steps+=1
+    result = []
     for i in range(number_of_rows):
-        autovalores.append(matrix[i][i])
-
-    print("Autovalores: " + str(autovalores))
-    print("Autovetores: " + str(identity_matrix))
+            result.append((i+1, matrix[i][i], identity_matrix[i]))
+            print(str(i+1) + "° Autovalor: " + str(matrix[i][i]) + ", Autovetor: " + str(identity_matrix[i]))
+            
+    print("Steps: "+ str(steps))
+    
+    return result
