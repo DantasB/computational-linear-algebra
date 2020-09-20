c76d59954d3ea373cbe7b4e04551590f83bbe2fe
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-09-20 00:44:56 -0300

    [List 3 ] Adding MMQ Function. Done.

--- List 3/ALC_List3.py
+++ List 3/ALC_List3.py
@@ -1,4 +1,8 @@
 import Matrix_Utils
 
-def linear_regression(matrix_x, matrix_y):
-    pass
+def mmq(vector_x, vector_y):
+    matrix_p            = Matrix_Utils.build_mmq_p_matrix(vector_x);
+    matrix_p_transposed = Matrix_Utils.get_transposed_matrix(matrix_p)
+    matrix_a            = Matrix_Utils.multiply_matrixes(matrix_p_transposed, matrix_p)
+    matrix_c            = Matrix_Utils.multiply_matrix_vector(matrix_p_transposed, vector_y)
+    return ALC_List1.solve_matrix(matrix_a, matrix_c, False);
