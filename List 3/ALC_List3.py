1e35fff59c9acdfaeee39d4b9881314dd6271f37
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-09-22 02:43:32 -0300

    Update ALC_List3.py

--- List 3/ALC_List3.py
+++ List 3/ALC_List3.py
@@ -1,4 +1,7 @@
-import Matrix_Utils
+from importlib.machinery import SourceFileLoader
+
+Matrix_Utils = SourceFileLoader("Matrix_Utils", "/home/bdantas/Área de Trabalho/ALC_Lists/Utils/Matrix_Utils.py").load_module()
+ALC_List1    = SourceFileLoader("ALC_List1", "/home/bdantas/Área de Trabalho/ALC_Lists/List_1/ALC_List1.py").load_module()
 
 def mmq(vector_x, vector_y):
     matrix_p            = Matrix_Utils.build_mmq_p_matrix(vector_x);
