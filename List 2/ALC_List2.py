71510bfee329887eb5cfd79ca1a197e6eb4390a7
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-09-22 02:41:42 -0300

    Update ALC_List2.py

--- List 2/ALC_List2.py
+++ List 2/ALC_List2.py
@@ -1,6 +1,7 @@
-import Matrix_Utils
 import math
+from importlib.machinery import SourceFileLoader
 
+Matrix_Utils = SourceFileLoader("Matrix_Utils", "/home/bdantas/Área de Trabalho/ALC_Lists/Utils/Matrix_Utils.py").load_module()
 
 def power_method(matrix):
     number_of_rows = len(matrix)
@@ -9,14 +10,14 @@ def power_method(matrix):
     y_vector      = Matrix_Utils.multiply_matrix_vector(matrix, eigenvector)
     first_element = 1
     eigenvalue    = y_vector[0]
-    steps             = 1
+    steps         = 1
 
     for i in range(number_of_rows):
         y_vector[i] = y_vector[i]/eigenvalue
         eigenvector = y_vector
 
     residue = math.fabs(eigenvalue - first_element)/eigenvalue
-    tol     = 10**-3
+    tol     = 10**-5
 
     while (residue>=tol):
         first_element = eigenvalue
@@ -25,8 +26,8 @@ def power_method(matrix):
 
         for i in range(number_of_rows):
             y_vector[i] = y_vector[i]/eigenvalue
-            eigenvector = y_vector
 
+        eigenvector = y_vector
         residue = math.fabs(eigenvalue-first_element)/eigenvalue
 
         steps+=1
@@ -41,8 +42,8 @@ def jacobi_method(matrix):
         return -1
 
     number_of_rows  = len(matrix)
-    identity_matrix = [[float(i==j) for j in range(number_of_rows)] for i in range(number_of_rows)]
-    tol             = 10**(-3)
+    eigenvector     = [[float(i==j) for j in range(number_of_rows)] for i in range(number_of_rows)]
+    tol             = 10**(-5)
     biggest_element = Matrix_Utils.get_biggest_element(matrix)
     steps           = 1
 
@@ -50,15 +51,15 @@ def jacobi_method(matrix):
         p_matrix            = Matrix_Utils.calculate_p_matrix(matrix, biggest_element)
         p_matrix_transposed = Matrix_Utils.get_transposed_matrix(p_matrix)
         matrix              = Matrix_Utils.multiply_matrixes(p_matrix_transposed, Matrix_Utils.multiply_matrixes(matrix, p_matrix))
-        identity_matrix     = Matrix_Utils.multiply_matrixes(identity_matrix, p_matrix)
+        eigenvector         = Matrix_Utils.multiply_matrixes(eigenvector, p_matrix)
         biggest_element     = Matrix_Utils.get_biggest_element(matrix)
 
         steps+=1
     result = []
     for i in range(number_of_rows):
-            result.append((i+1, matrix[i][i], identity_matrix[i]))
-            print(str(i+1) + "° Autovalor: " + str(matrix[i][i]) + ", Autovetor: " + str(identity_matrix[i]))
-            
+            result.append((i+1, matrix[i][i], eigenvector[i]))
+            print(str(i+1) + "° Eigenvalue: " + str(matrix[i][i]) + ", Eigenvector: " + str(eigenvector[i]))
+
     print("Steps: "+ str(steps))
-    
+
     return result
